c3cf304dbdf078f8d6fc3a043ff5105e2f4576c5
rexto <rexto@10.5.13.195>
2017-11-23 17:51:40 -0500

    Añadi la linea 3

---
+++ EjercicioGit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Yoaner
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hola Richar");
+        JOptionPane.showMessageDialog(null, "Hola Richar2");
+        JOptionPane.showMessageDialog(null, "Hola Richar3");
+        
+    }
+    
+}
